80b5ee1392297bcab1b11f16ff448760c879291a
Vakacharla-Lokesh <81227751+Vakacharla-Lokesh@users.noreply.github.com>
2023-06-06 20:45:52 +0530

    Add files via upload

---
+++ casear_cipher.py
@@ -0,0 +1,51 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+
+def caesar(caesar_text, shift_amount, type):
+    message = ""
+    if (type.lower() == "decode"):
+        shift_amount *= -1
+    for char in caesar_text:
+        if char in alphabet:
+            index = alphabet.index(char)
+            new_position = index + shift_amount    
+            message += alphabet[new_position]
+        else:
+            message += char
+    print(f"The {type.lower()}d message is: {message}")
+
+should_continue = True   
+while should_continue:
+    direction = input("type 'encode' to encrypt, type 'decode' to decrypt: ")
+    text = input("Type your message: \n").lower()
+    shift = int(input("Type the shift number: \n"))
+
+    shift = shift % 25
+        
+    caesar(text, shift, direction)
+
+    result = input("Type 'yes' if you wish to run again. Otherwise type 'no'.\n")
+    if result.lower() == "yes":
+        should_continue = True
+    else :
+        should_continue = False
+        print("Thank you for using me")
